20e2879832f0154e235aac7f6bbe24b44eae158c
kylevoth <kyle_voth@hotmail.com>
2016-11-06 19:15:41 -0600

    Part 2 Commit

---
+++ app/controllers/finder_controller.rb
@@ -0,0 +1,18 @@
+class FinderController < ApplicationController
+  #before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
+  # GET /customers
+  # GET /customers.json
+  def index
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @customers = Customer.all.order(:full_name)
+  end
+
+  def missing_email
+    @customers = Customer.where("email_address == ''")
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :customers
+
+  get '' => 'finder#index', as: 'index'
+
+  get 'alphabetized' => 'finder#alphabetized'
+
+  get 'missing_email' => 'finder#missing_email', as: 'missing_email'
+
+  root to: 'finder#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
